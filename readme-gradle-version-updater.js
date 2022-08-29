2f3179a7fd2ef12368bd25ca06b996b2500f08a1
Paul Hagedorn <panzer1119@gmail.com>
2022-08-29 22:11:46 +0200

    build: rename version updater to readme-gradle-version-updater.js and readme-maven-version-updater.js and improve them

--- readme-gradle-version-updater.js
+++ readme-gradle-version-updater.js
@@ -1,17 +1,17 @@
 function versionRegex (contents) {
-    return RegExp('<version>([\\d\\.]+)</version>', 'm')
+    return RegExp('implementation ["\']de\.codemakers\.netbox:netbox-java-client:(.+?)["\']', 'm')
 }
 
 module.exports.readVersion = function (contents) {
     const matches = versionRegex(contents).exec(contents)
     if (matches === null) {
-        throw new Error('Failed to read the version field in your xml file - is it present?')
+        throw new Error('Failed to read the gradle version field in your README.md file - is it present?')
     }
     return matches[1]
 }
 
 module.exports.writeVersion = function (contents, version) {
     return contents.replace(versionRegex(contents), () => {
-        return `<version>${version}</version>`
+        return `implementation "de.codemakers.netbox:netbox-java-client:${version}"`
     })
 }
--- readme-maven-version-updater.js
+++ readme-maven-version-updater.js
@@ -1,17 +1,17 @@
 function versionRegex (contents) {
-    return RegExp('implementation "de.codemakers.netbox:netbox-java-client:([\\d\\.]+)"', 'm')
+    return RegExp('<version>(.+?)<\/version>', 'm')
 }
 
 module.exports.readVersion = function (contents) {
     const matches = versionRegex(contents).exec(contents)
     if (matches === null) {
-        throw new Error('Failed to read the version field in your xml file - is it present?')
+        throw new Error('Failed to read the maven version field in your README.md file - is it present?')
     }
     return matches[1]
 }
 
 module.exports.writeVersion = function (contents, version) {
     return contents.replace(versionRegex(contents), () => {
-        return `implementation group: "de.codemakers", name: "radarr4j", version: "${version}"`
+        return `<version>${version}</version>`
     })
 }
